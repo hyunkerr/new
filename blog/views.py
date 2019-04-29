afc4d0cac802e821f87af2687d2bd76109285869
“”hyunkerr <shg7788@likelion.org>
2019-04-29 11:36:19 +0000

    gogo

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from djang.utils import timezone
+from .forms import PostForm
 from .models import Post
 def home(request):
     posts = Post.objects.all
     return render(request, 'home.html', {'posts':posts})
 
+def new(request):
+    if request.method == "POST"
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('home')
+    else:
+        form = PostForm()
+    return render(request, 'new.html', {'form':form})
 # Create your views here.
--- gitgit/urls.py
+++ gitgit/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 from blog import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home, name='home')
+    url(r'^$', views.home, name='home'),
+    url(r'^new/$', views.new, name='new'),
 ]
